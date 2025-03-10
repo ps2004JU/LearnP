404385352d89a9d7fb5b2940c697382cda4d4860
ps2004JU <punya.22bcan062@jecrcu.edu.in>
2025-03-10 14:08:25 +0530

    s

--- 1.py
+++ 1.py
@@ -1,9 +1,16 @@
 import pandas as pd
 a=pd.read_csv("a1.csv")
 count=0
+x=""
+top_count=0
 for i in a["Average"]:
     if(i>39.99):
-        print(f"{a.loc[count,"Name "]} has passed")
+        print(f"{a.loc[count,"Name ]} has passed")
     else:
-        print(f"Bad luck {a.loc[count,"Name "]} has failed")
+        if(i==max(a["Average"])):
+            top_count=count
+        print(f"Bad luck {a.loc[count,"Name"]} has failed")
     count+=1
+
+print(f"{a.loc[top_count,"Name"]} is the topper with highest grade as {max(a["Average"])}")
+print("Thank you for sure")
