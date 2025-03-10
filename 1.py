6d92192ba7b0b447c89bbb247ebd4252a837eb23
ps2004JU <punya.22bcan062@jecrcu.edu.in>
2025-03-10 14:52:55 +0530

    Sc

--- 1.py
+++ 1.py
@@ -5,12 +5,21 @@ x=""
 top_count=0
 for i in a["Average"]:
     if(i>39.99):
-        print(f"{a.loc[count,"Name"]} has passed")
-    else:
         if(i==max(a["Average"])):
+<<<<<<< HEAD
+            print(f"Congratulations !!! {a.loc[count,"Name"]} has topped with grade {i}")
+        else:
+            print(f"{a.loc[count,"Name"]} has passed with grade {i}")
+        
+    else:
+        print(f"{a.loc[count,"Name"]} got a KT, good luck for the next time")
+    count+=1
+
+print("Note, a student gets KT when his grade is less than 39.9")
+=======
             top_count=count
-        print(f"Bad luck {a.loc[count,"Name"]} has failed")
+        print(f"Bad luck {a.loc[count,"Name"]} got a KT, good")
     count+=1
 
 print(f"{a.loc[top_count,"Name"]} is the topper with highest grade as {max(a["Average"])}")
-print("Thank you for sure")
+>>>>>>> main
